1656528a1d4dfcfc1d9bf866d909f26494e0f322
Steve Golton <steve@inovorobotics.com>
2021-07-27 18:10:59 +0100

    Updated robot example to just enable/disable the arm

--- commander_api/scripts/robot_example.py
+++ commander_api/scripts/robot_example.py
@@ -2,47 +2,13 @@
 import rospy
 import math
 from commander_api.robot_client import RobotClient
-from commander_api.psu_client import  PSUClient
-from commander_api.io_client import  IOClient
-from commander_api.custom_datatypes import *
-
-pi = math.pi
+import time
 
 rospy.init_node('robot_example')
-robot = RobotClient()
-psu = PSUClient()
-io = IOClient()
-
-# psu.bus_power_off()
-
-# psu.bus_power_on()
+robot = RobotClient('/robot')
 
 robot.disable()
+
+time.sleep(1.0)
+
 robot.enable()
-
-
-
-# #set digital output on the wrist to high
-io.wrist_digital_write(channel = 0, value = 1)
-wrist_value = io.wrist_digital_read(channel = 0)
-# #read the digital input on the wrist
-# wrist_value = robot.wrist_digital_read(pin = 0)
-# print('Digital input on the wrist: ', wrist_value)
-
-# #print the latest error message
-error=robot.get_errors()
-print(error.message)
-# #set digital output on the psu to high
-# psu.digital_write(pin=0, value = 1)
-robot.clear_errors()
-error=robot.get_errors()
-print(error.message)
-# #read the digital input of the psu
-# psu_value = psu.dig
-# ital_read(pin = 0)
-# print('Digital input in the psu: ', psu_value)
-
-
-
-#turn off the psu
-# psu.power_off()
--- commander_api/src/commander_api/robot_client.py
+++ commander_api/src/commander_api/robot_client.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
 import rospy
-from arm_msgs.msg import *
-from std_msgs import *
 from std_srvs.srv import Trigger
-from error_logger.srv import GetErrors
-from error_msgs.msg import *
-from commander_api.custom_datatypes import *
-
-import math
-import time
-
-pi = math.pi
 
 class RobotClient:
     """Robot client class used for basic controll of the robot functionality
     """    
-    def __init__(self):
-        self.disable_srv = rospy.ServiceProxy('/robot/disable', Trigger)
-        self.enable_srv = rospy.ServiceProxy('/robot/enable', Trigger)
-        self.get_errors_srv = rospy.ServiceProxy('/get_errors', GetErrors)
-        self.clear_errors_srv = rospy.ServiceProxy('/clear_errors', Trigger)
+    def __init__(self, ns='/robot'):
+        self.disable_srv = rospy.ServiceProxy(ns + '/disable', Trigger)
+        self.enable_srv = rospy.ServiceProxy(ns + '/enable', Trigger)
         
     
     def enable(self):
@@ -39,30 +27,3 @@ class RobotClient:
         res = self.disable_srv()
         if res.success == False:
             raise Exception("Failed to disable the robot")
-
-
-
-    def get_errors(self):
-        """Print the latest error message
-
-        :return: Error message
-        :rtype: ErrorMessage
-        """    
-        res = self.get_errors_srv()
-        error = ErrorMessage()
-        if len(res.errors) > 0:
-            error.message = res.errors[-1].message
-            error.error_code = res.errors[-1].error_code
-            error.source = res.errors[-1].source
-
-        return error
-        
-
-    def clear_errors(self):
-        """Clear all errors 
-
-        :raises Exception: Failed to clear errors
-        """           
-        res = self.clear_errors_srv()
-        if res.success == False:
-            raise Exception("Failed to clear errors")
