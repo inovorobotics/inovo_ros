9d473d75dd3aaf4b743604553680387939a9e18d
Steve Golton <steve@inovorobotics.com>
2021-07-29 13:29:02 +0100

    Moved some examples into the examples dir

--- commander_api/scripts/examples/enable_disable_robot.py
+++ commander_api/scripts/examples/enable_disable_robot.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 import rospy
-import math
-from commander_api.robot_client import RobotClient
 import time
+from commander_api.robot_client import RobotClient
 
 rospy.init_node('robot_example')
 robot = RobotClient('/robot')
 
 robot.disable()
-
 time.sleep(0.5)
-
 robot.enable()
-
-robot.switch_controller("trajectory")
